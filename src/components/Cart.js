6dca9f5c61d26dc434395fb285ad98a49306f8c4
Fran <fmjerez@icloud.com>
2020-07-23 11:25:51 +0200

    localStorage, useEffect & PropTypes done

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,21 +1,39 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
 
 const Cart = ({ handleFavorite, food, handleDelete }) => {
-  const { article, quantity, person, date, coments, id } = food;
+  const { item, quantity, person, date, coments, id } = food;
 
   return (
     <Fragment>
       <div className="cita">
-        <h4>{article}</h4>
-        <p>Quantity: {quantity}</p>
+        <h4>{item}</h4>
+        <p className="u-full-width">Quantity: {quantity}</p>
         <p>Person who added: {person}</p>
         <p>Date: {date}</p>
         <p>Coments: {coments}</p>
-        <button onClick={() => handleDelete(id)}>Delete Article</button>
-        <button onClick={() => handleFavorite(id)}>Add to Favorite</button>
+        <button
+          className="u-full-width button add-button"
+          onClick={() => handleFavorite(id)}
+        >
+          Add to Favorite
+        </button>
+        <button
+          className="u-full-width button eliminar"
+          onClick={() => handleDelete(id)}
+        >
+          Delete item
+        </button>
       </div>
     </Fragment>
   );
+};
+
+Cart.propTypes = {
+  addFoodtoCart: PropTypes.func.isRequired,
+  handleFavorite: PropTypes.func.isRequired,
+  food: PropTypes.object.isRequired,
+  handleDelete: PropTypes.func.isRequired,
 };
 
 export default Cart;
